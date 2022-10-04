7d7076306d03cb3351a8b0350e3bf39280cbcc68
Bence-98 <71707990+Bence-98@users.noreply.github.com>
2022-10-04 22:30:05 +0200

    Platform texture

--- app/src/main/java/com/example/szakdolgozat/Platform.java
+++ app/src/main/java/com/example/szakdolgozat/Platform.java
@@ -3,16 +3,20 @@ package com.example.szakdolgozat;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Shader;
 
 public class Platform implements GameObject {
 
     private Rect platform;
     private int color;
     private Bitmap bricks;
+    private Paint paint;
+    private int pRight, pTop;
 
 
     public Rect getPlatform() {
@@ -21,11 +25,18 @@ public class Platform implements GameObject {
 
 
     public Platform(int pLeft, int pTop, int pRight, int pBottom, int color) {
+        this.pRight = pRight;
+        this.pTop = pTop;
+
         this.color = color;
         platform = new Rect(pLeft, pTop, pRight, pBottom);
         bricks = BitmapFactory.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.brick);
 
         bricks = Bitmap.createBitmap(bricks);
+
+        paint = new Paint();
+
+        paint.setShader(new BitmapShader(bricks, Shader.TileMode.REPEAT,Shader.TileMode.REPEAT));
 
         //bricks = Bitmap.createScaledBitmap(bricks,platform.right-platform.left,platform.bottom-platform.top,false);
 
@@ -37,12 +48,17 @@ public class Platform implements GameObject {
 
     @Override
     public void draw(Canvas canvas) {
-        Paint paint = new Paint();
-        paint.setColor(color);
-        canvas.drawRect(platform, paint);
+        //Paint paint = new Paint();
+        /*paint.setColor(color);
+        canvas.drawRect(platform, paint);*/
+        // paint.setShader(new BitmapShader(bricks, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT));
+
+
+        //canvas.translate(pRight,pTop);
         //animationManager.draw(canvas,platform);
         //canvas.drawBitmap(bricks,platform.left,platform.top,null);
-        canvas.drawRect(0,100,21,21, paint);
+        //canvas.drawRect(0,0,21,21, paint);
+        canvas.drawRect(platform,paint);
     }
 
 
